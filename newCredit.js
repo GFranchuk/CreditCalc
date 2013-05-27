94d4a65cba1a1a14a57020e327478e00b99f86ae
GFranchuk <copper93@mail.ru>
2013-05-27 11:29:30 +0600

    Франчук ОМГ бох дот нета

---
+++ newCredit.js
@@ -0,0 +1,77 @@
+calculateCredit = (
+
+	function() {
+	
+ 		var 
+ 			errMsg = "Ошибочка. Проверьте введённые цифры",
+			wrongResult = {
+				overpay: errMsg, 
+				payments: [errMsg],
+				total: errMsg 
+			}
+		;
+		
+		return {
+		
+			differ: function(sum, term, rate) {
+			 	var 
+			 		overpay, payment, count, x,
+			 		result = { 
+						total: 0,
+						overpay: 0,
+						payments: [ ]
+					}
+			 	;
+	
+				for(var x in arguments) {
+					arguments[x] = parseFloat(arguments[x]);
+					if( isNaN( arguments[x] ) ) return wrongResult;
+					if( arguments[x] === 0) arguments[x] = 0.0000000000001;
+				}
+
+				if(rate) rate = rate / 100 / 12; 
+				while(term) {
+					payment = sum / term;
+					overpay =  sum * rate;
+					result.overpay += overpay;
+					count = result.payments.length; 
+					result.payments.push( +(payment + overpay ).toFixed( 2 ) );
+					result.total += result.payments[count];
+					term--;
+					sum -= payment;
+				}
+				result.total = result.total.toFixed(2);
+				result.overpay = result.overpay.toFixed(2);
+				return result
+				
+			},
+			
+			annuitet: function(sum, term, rate) {
+				var 
+					x, koeffAnn	,
+					result = { 
+						total: 0,
+						overpay: 0,
+						payments: [ ]
+					}
+				;
+	
+				for(var x in arguments) {
+					arguments[x] = parseFloat(arguments[x]);
+					if( isNaN( arguments[x] ) ) return wrongResult;
+					if( arguments[x] === 0) arguments[x] = 0.0000000000001;
+				}
+				
+				if(rate) rate = rate / 100 / 12; 
+				koeffAnn = rate * Math.pow( ( 1 + rate ), term ) / (  Math.pow( ( 1 + rate ), term ) - 1 );
+				result.payments.push( ( sum * koeffAnn ).toFixed( 2 ) );
+				result.total = ( result.payments[0] * term ).toFixed( 2 );
+				result.overpay = ( result.total - sum ).toFixed( 2 );
+
+				return result;
+			}
+		}//object
+		
+	}//anonymous function
+	
+)()
